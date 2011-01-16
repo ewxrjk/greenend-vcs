274957ce87ed383c9a34ec594e73dd723094506c
Richard Kettlewell <rjk@greenend.org.uk>
2011-01-16 14:18:12 +0000

    copyright date

--- src/execute.cc
+++ src/execute.cc
@@ -1,6 +1,6 @@
 /*
  * This file is part of VCS
- * Copyright (C) 2009 Richard Kettlewell
+ * Copyright (C) 2009, 2011 Richard Kettlewell
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
