fc46ed9faa2798c8a39a66bce70bfed40f6a23f8
Richard Kettlewell <rjk@greenend.org.uk>
2014-05-19 22:56:23 +0100

    Add missing includes

--- src/vcs.h
+++ src/vcs.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of VCS
- * Copyright (C) 2009-2011 Richard Kettlewell
+ * Copyright (C) 2009-2011, 2014 Richard Kettlewell
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,6 +29,7 @@
 #include <cstring>
 #include <cassert>
 #include <cstdarg>
+#include <clocale>
 #include <getopt.h>
 #include <errno.h>
 #include <sys/types.h>
